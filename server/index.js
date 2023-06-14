51b7facd839c01d6c8a2177996ab962294b354dc
Anjan Talatam <tspa2117@gmail.com>
2023-06-14 13:01:47 +0530

    👌 IMPROVE: make register route work

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.use(cors());
+
+// this parses the body to json
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.send("Server is up");
+});
+
+app.post("/api/register", (req, res) => {
+  console.log(req.body);
+  res.json({ status: "ok" });
 });
 
 app.listen(port, () => {
